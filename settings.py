74ab610030b1f00f98d104f7efb5fe7cd8c7b676
Александр Стецуро <a.stetsuro@a.stetsuro.dc.rk>
2021-08-30 16:17:02 +0300

    Initial commit

---
+++ settings.py
@@ -0,0 +1,39 @@
+class Settings():
+    def __init__(self):
+        # Параметры экрана.
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (255, 255, 255)
+
+        # Скорость корабля.
+        self.ship_limit = 3
+
+        # Параметры снаряда.
+        self.bullet_speed = 1.5
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
+        self.bullets_allowed = 3
+
+        # Скорость пришельцев
+        self.fleet_drop_speed = 10
+
+        # Темп ускорения игры
+        self.speedup_scale = 1.1
+        self.score_scale = 1.5
+        self.initialize_dynamic_settings()
+
+    def initialize_dynamic_settings(self):
+        self.ship_speed_factor = 1.5
+        self.bullet_speed_factor = 3.0
+        self.alien_speed_factor = 0.1
+        self.fleet_direction = 1
+        self.alien_points = 50
+
+    def increase_speed(self):
+        self.ship_speed_factor *= self.speedup_scale
+        self.bullet_speed_factor *= self.speedup_scale
+        self.alien_speed_factor *= self.speedup_scale
+
+        self.alien_points = int(self.alien_points * self.score_scale)
+
